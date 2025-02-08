9acd7762883bb5c8867afc2249f83342d318c1fd
danielsones <daniel.sones123@gmail.com>
2025-02-08 22:28:28 +0000

    feat: chat bubbles

--- frontend/src/home/ChatArea.tsx
+++ frontend/src/home/ChatArea.tsx
@@ -1,6 +1,7 @@
-import { TextField } from "@mui/material";
+
 import ChatBar from "./components/ChatBar";
 import styled from "styled-components";
+import Chat from "./components/Chat";
 
 const MessageArea = styled.div`
   height: 100%;
@@ -10,7 +11,8 @@ const ChatArea = () => {
   return (
     <>
       <MessageArea>
-        <p>Yo This is epic chat area bruv</p>
+        <Chat isSent={false} message="Wow UniVerse is the best chat app ever made"/>
+        <Chat isSent={true} message="So true"/>
       </MessageArea>
       <ChatBar />
     </>
