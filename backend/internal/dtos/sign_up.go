77d42beee6708b6afd3c86a8cec20e5dd267ddc5
danielsones <daniel.sones123@gmail.com>
2025-02-13 17:07:02 +0000

    add missing files

--- backend/internal/dtos/sign_up.go
+++ backend/internal/dtos/sign_up.go
@@ -1,7 +1,8 @@
 package dtos
 
 type CreateUserRequest struct {
-	Username string `json:"username" binding:"required"`
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=6"`
+	Username        string `json:"username" binding:"required"`
+	Email           string `json:"email" binding:"required,email"`
+	Password        string `json:"password" binding:"required,min=6"`
+	ConfirmPassword string `json:"confirm_password" binding:"required,min=6"`
 }
