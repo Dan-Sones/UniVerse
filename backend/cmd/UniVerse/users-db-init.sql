b7109d791cbe5cd03158929849fa003539a94b5c
danielsones <daniel.sones123@gmail.com>
2025-02-13 22:04:29 +0000

    fix: error on redeploy

--- backend/cmd/UniVerse/users-db-init.sql
+++ backend/cmd/UniVerse/users-db-init.sql
@@ -1,7 +1,9 @@
-CREATE SCHEMA auth;
-CREATE TABLE auth.users (
-                            id SERIAL PRIMARY KEY,
-                            username VARCHAR(50) UNIQUE NOT NULL,
-                            email VARCHAR(100) UNIQUE NOT NULL,
-                            password TEXT NOT NULL
+CREATE SCHEMA IF NOT EXISTS auth;
+
+CREATE TABLE IF NOT EXISTS auth.users
+(
+    id       SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE  NOT NULL,
+    email    VARCHAR(100) UNIQUE NOT NULL,
+    password TEXT                NOT NULL
 );
