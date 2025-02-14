9faf36076475b49816c58c5c07c15bf96bde6a6f
danielsones <daniel.sones123@gmail.com>
2025-02-14 23:55:52 +0000

    fix: remove unused infra

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -49,10 +49,6 @@ resource "aws_api_gateway_method_settings" "rate_limit" {
 resource "aws_api_gateway_deployment" "chat_api_deploy" {
   depends_on  = [aws_api_gateway_integration.proxy_integration]
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-
-  triggers = {
-    redeployment = timestamp()
-  }
 }
 
 resource "aws_api_gateway_stage" "prod_stage" {
--- terraform/security.tf
+++ terraform/security.tf
@@ -1,36 +1,3 @@
-resource "aws_security_group_rule" "allow_ssh" {
-  security_group_id = var.security_group_id
-
-  type        = "ingress"
-  from_port   = 22
-  to_port     = 22
-  protocol    = "tcp"
-  cidr_blocks = ["${var.my_ip}/32"]
-}
-
-resource "aws_security_group_rule" "allow_api" {
-  security_group_id = var.security_group_id
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["${var.my_ip}/32"]
-}
-
-resource "aws_security_group_rule" "allow_public_api" {
-  security_group_id = var.security_group_id
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["52.216.0.0/15"]
-}
-
-
-data "aws_instance" "go_backend" {
-  instance_id = aws_instance.go_backend.id
-}
-
 resource "aws_security_group_rule" "allow_api_gateway_to_alb" {
   security_group_id = aws_security_group.alb_sg.id
   type              = "ingress"
